686e943b059ff6bf25d4775580c7c1b6167f4f85
howwant <corn1128@naver.com>
2022-12-20 15:32:53 +0900

    yonsei-hospital

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+  $('#mainBanner').slick({
+    slide: 'div',
+    infinite: true,
+    dots: true,
+    autoplay : true,
+    speed: 200,
+    autoplaySpeed : 5000,
+    pauseOnHover : true,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
+  $('.img').slick({
+    slide: 'div',
+    autoplay : true,
+    infinite: true,
+    speed: 500,
+    dots: true,
+    fade: true,
+    cssEase: 'linear',
+    arrows : true
+  });
+  $("#banner_box #mainBanner .slick-prev").append(
+    '<i class="xi-angle-left-min"></i>'
+  );
+  $("#banner_box #mainBanner .slick-next").append(
+    '<i class="xi-angle-right-min"></i>'
+  );
+  $("#ad .img .slick-prev").append(
+    '<i class="xi-angle-left-min"></i>'
+  );
+  $("#ad .img .slick-next").append(
+    '<i class="xi-angle-right-min"></i>'
+  );
+});
